7141a644be20011a6bf3d9613085fcc34a69b0c1
William Lennartsson <william.j.lennartsson@gmail.com>
2020-02-26 13:03:36 +0100

    Initial Commit

---
+++ coredataTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coredataTest2
+//
+//  Created by William Lennartsson on 2020-02-26.
+//  Copyright © 2020 William Lennartsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
