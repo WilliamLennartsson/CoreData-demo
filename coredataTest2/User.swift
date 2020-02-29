cc01f293997fd21d4a6bb8233ffabb9b924348a6
William Lennartsson <william.j.lennartsson@gmail.com>
2020-02-29 15:01:51 +0100

    first commit

---
+++ coredataTest2/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  coredataTest2
+//
+//  Created by William Lennartsson on 2020-02-26.
+//  Copyright © 2020 William Lennartsson. All rights reserved.
+//
+
+import UIKit
+
+class User: NSManagedObject {
+
+}
